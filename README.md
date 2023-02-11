e8a02408d85913ea72aa14d76e927bd61be41cb8
Akram Hossain <akram.hossain.cse.cu@gmail.com>
2023-02-12 01:12:32 +0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# asdfgh
+# EnRel-G
+
+A Lightweight System for Improving the domain-specific Named Entity Recognition 
+
+## Training & Prediction
+1. Put the downloaded [BioBERT](https://github.com/dmis-lab/biobert) to *./pybiobert_base* and [SciBERT](https://github.com/allenai/scibert) to *./scibert_scivocab_uncased* 
+2. Put the conll-style data to *./data*. If you want to to change the dataset, make sure to change the parameter *--data_dir* and the file names in *data_utils.py*
+3. run the following commands or run the shell file: *sh run.sh*
+
+```
+
+python run_ner.py --data_dir=data/AnatEM --bert_model=pybiobert_base --task_name=ner --max_seq_length=128 --num_train_epochs=100 --learning_rate=5e-5 --train_batch_size=32 --eval_batch_size=32 --do_train --do_eval --do_predict --seed=42 --use_rnn --use_crf --use_gat --gat_type=AF --fuse_type=v --do_lower_case --relearn_embed --warmup_proportion=0.1
+
+```
+
+## Citation
+
+If you use this system, please cite the paper where it was introduced.
+
+[paper link](https://aclanthology.org/2021.acl-short.93.pdf) 
+```text
+@inproceedings{chen-etal-2021-explicitly,
+ author = {Chen, Pei  and Ding, Haibo  and Araki, Jun  and Huang, Ruihong},
+ booktitle = {ACL-2021},
+ publisher = {Association for Computational Linguistics},
+ title = {Explicitly Capturing Relations between Entity Mentions via Graph Neural Networks for Domain-specific Named Entity Recognition},
+ url = {https://aclanthology.org/2021.acl-short.93},
+ year = {2021}
+}
+```
+
---
+++ gcn.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GCNLayer(nn.Module):
+    def __init__(self, in_features, out_features, use_bias=True):
+        super(GCNLayer, self).__init__()
+        self.weight = nn.Parameter(torch.FloatTensor(torch.zeros(size=(in_features, out_features))))
+        self.a1 = nn.Parameter(torch.zeros(size=(out_features, 1)))
+        if use_bias:
+            self.bias = nn.Parameter(torch.FloatTensor(torch.zeros(size=(out_features,))))
+        else:
+            self.register_parameter('bias', None)
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        nn.init.xavier_uniform_(self.weight)
+        if self.bias is not None:
+            nn.init.zeros_(self.bias)
+
+    def forward(self, x, adj):
+        x = x @ self.weight
+        if self.bias is not None:
+            x += self.bias
+        print(f"\nx = {x.size()} \t adj = {adj.size()}")
+        return torch.mul(adj, x)
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+
+class GCN(nn.Module):
+    def __init__(self, node_features, hidden_dim, num_classes, dropout, use_bias=True):
+        super(GCN, self).__init__()
+        self.gcn_1 = GCNLayer(node_features, hidden_dim, use_bias)
+        self.gcn_2 = GCNLayer(hidden_dim, num_classes, use_bias)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def initialize_weights(self):
+        self.gcn_1.initialize_weights()
+        self.gcn_2.initialize_weights()
+
+    def forward(self, x, adj):
+        x = F.relu(self.gcn_1(x, adj))
+        print(f"\nx2 = {x.size()} \t adj = {adj.size()}")
+        x = self.dropout(x)
+        x = self.gcn_2(x, adj)
+        print(f"\nx3 = {x.size()} \t adj = {adj.size()}")
+        return x
+
+
+
