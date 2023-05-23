eba21fe75b0c402c183e49a5e4d406084630b5e1
Akram Hossain <akram.hossain.cse.cu@gmail.com>
2023-05-24 05:01:16 +0600

    Update trainer2.py

--- gnner/trainer2.py
+++ gnner/trainer2.py
@@ -5,52 +5,6 @@ import pickle
 import pandas as pd
 from utils import compute_metrics, validate, extract_spans, num_ovelap_span
 
-def log_confusion_matrix(y_true=None, y_pred=None, labels=None, true_labels=None,
-                            pred_labels=None, normalize=False):
-    """                   
-    Computes the confusion matrix to evaluate the accuracy of a classification.
-    """
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
-    cm = confusion_matrix(y_true, y_pred)
-    if labels is None:
-        classes = unique_labels(y_true, y_pred)
-    else:
-        classes = np.asarray(labels)
-      
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        cm = np.around(cm, decimals=2)
-        cm[np.isnan(cm)] = 0.0
-            
-    if true_labels is None:
-        true_classes = classes
-    else:
-        true_label_indexes = np.in1d(classes, true_labels)
-        true_classes = classes[true_label_indexes]
-        cm = cm[true_label_indexes]
-            
-    if pred_labels is None:
-        pred_classes = classes
-    else:
-        pred_label_indexes = np.in1d(classes, pred_labels)
-        pred_classes = classes[pred_label_indexes]
-        cm = cm[:, pred_label_indexes]
-            
-    data=[]
-    count = 0
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if labels is not None and (isinstance(pred_classes[i], int)
-                                    or isinstance(pred_classes[0], np.integer)):
-            pred_dict = labels[pred_classes[i]]
-            true_dict = labels[true_classes[j]]
-        else:
-            pred_dict = pred_classes[i]
-            true_dict = true_classes[j]
-        data.append([pred_dict, true_dict, cm[i,j]])
-        count+=1
-    
-    return data      
 
 class LightningWrapper(pl.LightningModule):
 
@@ -112,6 +66,12 @@ class LightningWrapper(pl.LightningModule):
         
         if weighted_f1_score > self.best_wf1:
             self.best_wf1 = weighted_f1_score
+            optimizer = self.configure_optimizers
+            path = "/content/drive/MyDrive/best_model.pth"
+            torch.save({
+            "model_state_dict" :self.model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+        }, path)
         
 
         num_ov = num_ovelap_span(spans)
@@ -124,13 +84,6 @@ class LightningWrapper(pl.LightningModule):
         self.log('prec', p, prog_bar=True)
         self.log('rec', r, prog_bar=True)
         
-        data = log_confusion_matrix(true, pred, self.non_null_labels)
-        self.log({"confusion_matrix" : wandb.Table(
-                columns=['Predicted', 'Actual', 'Count'],
-                data=data)})
-        df = pd.DataFrame(data)
-        df.columns = ['Predicted', 'Actual', 'Count']
-        df.to_csv("prediction_file.csv")
 
     def test_step(self, val_batch, batch_idx):
         return self.validation_step(val_batch, batch_idx)
